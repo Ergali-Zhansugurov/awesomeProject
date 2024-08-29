4f04ec0024b2f2fd477f4a2281b0fc48c2edd4fe
Ergali-Zhansugurov <granbellsiki@gmail.com>
2024-08-29 12:07:05 +0500

    Rabbit за работал , но я уже не уверен нужен ли он мне разве что как связка между сервером и базой данных , но именно для кэша мне кажется это не к чему

---
+++ internal/models/Command.go
@@ -0,0 +1,17 @@
+package models
+
+const (
+	CommandDelete  RabbitCommand = "DELETE"
+	CommandPurge   RabbitCommand = "PURGE"
+	CommandAdd     RabbitCommand = "ADD"
+	CommandGet     RabbitCommand = "Get"
+	CommandGetByID RabbitCommand = "GetById"
+	CommandUpdate  RabbitCommand = "Update"
+)
+
+type RabbitCommand string
+
+type Msg struct {
+	Command RabbitCommand `json:"command"`
+	Key     interface{}   `json:"key"`
+}
